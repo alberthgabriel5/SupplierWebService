a5f2815a7813800abe985b4a38cc37de00e94b0c
alberthgabriel5 <alberthgabriel5@hotmail.com>
2017-02-21 22:25:23 -0600

    documentacion22-02-2017

--- Data/purchaseData.php
+++ Data/purchaseData.php
@@ -35,47 +35,7 @@ class purchaseData extends Data
         mysqli_close($conn);
         return $queryInsert;
         
-    }//fin function insertsupplier
-   /**
-     * Función que permite la obtención de todos los registros de 
-     * provedores de la base de datos
-     * @return array
-     */
-    function getAllPurchase() {
-
-        $conn = new mysqli($this->server, $this->user, $this->password, $this->db);
-        $conn->set_charset('utf8');
-        $result = mysqli_query($conn, "select * from tbpurchasingsupplier order by idPurchases asc");
-        $array = array();
-        while ($row = mysqli_fetch_array($result)) {
-            $currentData = new purchase($row['idSupplier'], $row['datePurchases'], $row['descriptionPurchases'],$row['product'],$row['totalPurchases']);
-            $currentData->setIdPurchase($row['idPurchases']);           
-            array_push($array, $currentData);
-             //echo ''.$currentData->getIdSupplier().' '.$currentData->getIdProduct().'<br>';
-        }
-        //echo 'obtuvo los valores';
-        //exit;
-        return $array;
-    }//fin función getSupplier
-    /**
-     * Función que permite la obtención de todos los registros de 
-     * un provedor de la base de datos
-     * Historico
-     * @return array Historico
-     */
-    function getAllPurchaseSupplier($idSupplier) {
-
-        $conn = mysqli_connect($this->server, $this->user, $this->password, $this->db);
-        $conn->set_charset('utf8');
-        $result = mysqli_query($conn, "select * from tbpurchasingsupplier where idSupplier=".$idSupplier." order by idPurchases asc");
-        $array = array();
-        while ($row = mysqli_fetch_array($result)) {
-            $currentData = new purchase($row['idSupplier'], $row['datePurchases'], $row['descriptionPurchases'],$row['product'],$row['totalPurchases']);
-            $currentData->setIdSupplier($row['idPurchases']);            
-            array_push($array, $currentData);
-        }
-        return $array;
-    }//fin función getSupplier    
+    }//fin function insertsupplier      
     function getNameSupplier($idSupplier){
         $conn = mysqli_connect($this->server, $this->user, $this->password, $this->db);
         $conn->set_charset('utf8');
